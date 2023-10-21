2ad9844d024bffac48aa29a1974f601e593a5923
Alejandro Jara G <ajaragamboa@gmail.com>
2023-10-21 11:19:02 -0600

    A

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,8 @@
 console.log("Entro index.js");
+
+fetch('https://dev4humans.com.mx/api/clases/productos',{method: "GET"})
+    .then(response => response.json())
+    .then(data =>{
+        console.log("La promesa se resolvio");
+        console.log(data);
+    } ).catch(error =>console.log(error))
